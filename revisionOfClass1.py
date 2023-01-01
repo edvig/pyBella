5417b9279e3d390da9de2d6679056703e757590e
Edvi Gergő Ádám <44545148+edvig@users.noreply.github.com>
2023-01-01 18:09:07 +0100

    Bella's solution

--- revisionOfClass1.py
+++ revisionOfClass1.py
@@ -1,7 +1,30 @@
 # 1. Print out "Hello World" on the console
+print ("hello world")
 
 # 2. Create a variable and name it however you like, assign your name as the value of this variable
-
+variableDegrees = "bella"
+print (variableDegrees)
 # 3. Create a "rolling dice": Make a variable that chooses a random value, like a rolling dice. Print out its value on the console.
-
+import random
+start = 1
+final = 6
+randomVariable = random.randrange(start, final)
+print("The random number:",randomVariable)
 # 4. Create a variable that chooses a value between 1 and 10. If the value is odd print "Odd", if its even, print "Even".
+start = 1
+stop = 10
+steps = 1
+randomVariable= random.randrange (start, stop, steps)
+if(1, 3, 5, 7, 9):
+    print ("The number is odd")
+else:
+    (2, 4, 6, 8, 10)
+    print("the number is even")
+# Extra
+bella = 100
+gregório = 99
+gregório +=2
+if(bella > gregório) :
+    print("quem vale mais é bella ")
+if(gregório > bella) :
+    print("quem vale mais é gregório")
