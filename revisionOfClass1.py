8734ec831e15f1024616367678d8bbd54af510fd
Edvi Gergő Ádám <44545148+edvig@users.noreply.github.com>
2023-01-01 17:19:16 +0100

    revision of first class

---
+++ revisionOfClass1.py
@@ -0,0 +1,7 @@
+# 1. Print out "Hello World" on the console
+
+# 2. Create a variable and name it however you like, assign your name as the value of this variable
+
+# 3. Create a "rolling dice": Make a variable that chooses a random value, like a rolling dice. Print out its value on the console.
+
+# 4. Create a variable that chooses a value between 1 and 10. If the value is odd print "Odd", if its even, print "Even".
